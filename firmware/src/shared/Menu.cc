2e306d4a67d0a72e571909a14f232feb5f81e806
Dan Newman <dan.newman@mtbaldy.us>
2013-03-05 16:28:52 -0800

    Update version number

--- firmware/src/shared/Menu.cc
+++ firmware/src/shared/Menu.cc
@@ -161,7 +161,7 @@ static void digits3(char *buf, uint8_t val)
 void SplashScreen::update(LiquidCrystal& lcd, bool forceRedraw) {
 	const static PROGMEM prog_uchar splash1[] = "  Sailfish FW   ";
 	const static PROGMEM prog_uchar splash2[] = " -------------- ";
-	const static PROGMEM prog_uchar splash3[] = "Thing 32084 4.1z";
+	const static PROGMEM prog_uchar splash3[] = "Thing 32084 4.3 ";
 	//    static PROGMEM prog_uchar splash4[] = " Revision 00000 ";
 	const static PROGMEM prog_uchar splash4[] = " Revision " SVN_VERSION_STR;
 
